f80aff2b8ee47b4ecb5e3d5ed0d7cd90fdc7c6f4
plm <plmcodes@gmail.com>
2025-09-04 18:01:10 +0530

    Add files via upload

---
+++ template-swindow.cpp
@@ -0,0 +1,14 @@
+/*
+    int s = 0 , e = 0;
+    while(e < n)
+    {
+       // traverse till the ending element of subarray
+       if(e-s+1==k)
+       {
+         // caclulate the answer
+         i++;
+       }
+       j++; // slide the window
+
+    }
+*/
